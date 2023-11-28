be82212b8836aa603f4d6afae02851537d84a883
Lizz <lizzcklein@gmail.com>
2023-11-28 09:23:29 -0500

    added a neighborhood class

--- server/models.py
+++ server/models.py
@@ -3,4 +3,10 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Neighborhood(db.Model, SerializerMixin):
+    __tablename__ = "neighborhoods"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+
+
